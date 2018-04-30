f04d574b92b9043fe71f6204b5ab80e8ec78fc6d
jarrodyellets <jarrod524@gmail.com>
2018-04-30 23:52:50 +0200

    Refactor

--- src/index.js
+++ src/index.js
@@ -21,25 +21,27 @@ class App extends React.Component {
 		this.solve = this.solve.bind(this);
 	}
 
+//Click handlers for each button
 	handleClick(symbol, id){
 		id == "function" ? this.setState((state) => ({
 			number: false,
 			math: !isNaN(state.math.slice(-1)) ? state.math + symbol : state.math.slice(0, -1) + symbol,
 			display: state.display
-		})) : null;
-		id == "number" ? this.changeCalc(symbol) : null;
-		id == "decimal" && this.state.display.indexOf(".") === -1 ? this.changeCalc(symbol) : null;
+			})) : null;
 		id == "clear" ? this.setState({
 			display: "0",
 			math: ""
-		}): null;
+			}): null;
 		id == "delete" ? this.setState((state) => ({
 			display: state.display.length > 1 ? state.display.substring(0, state.display.length - 1) : "0",
 			math: state.math.substring(0, state.math.length - 1)
-		})): null;
+			})): null;
+		id == "number" ? this.changeCalc(symbol) : null;
+		id == "decimal" && this.state.display.indexOf(".") === -1 ? this.changeCalc(symbol) : null;
 		id == "equals" ? this.solve(this.state.math) : null;
 	}
 
+//Click handler for equals
 	solve(string){
 		const answer = eval(string).toString();
 		this.setState((state) => ({
@@ -49,6 +51,7 @@ class App extends React.Component {
 		}))
 	}
 
+//Click handler for numbers
 	changeCalc(symbol){
 		this.state.display.length < 9 || this.state.answer ?
 		this.setState((state) => ({
@@ -57,13 +60,12 @@ class App extends React.Component {
 			number: true,
 			answer: false
 		})): null
-
 	}
 
 	render() {
 		return (
 				<div className="calculator">
-					<Screen display={this.state.display} />
+					<Screen display={this.state.display} hover={this.state.hover} />
 					<div className="panel">
 				    <div className="solar"></div>
 				    <div className="solar middle"></div>
